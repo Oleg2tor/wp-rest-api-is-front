a15ce8be0b118d1c4bae0355e5b0fc5766f60447
Oleg Kostin <Oleg2tor@gmail.com>
2016-03-19 23:51:51 +0300

    First public release

---
+++ wp-api-is-front.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Plugin Name: WP REST API (V2) isFront
+Description: WP REST API (V2) Modifications for isFront endpoint.
+Author: Oleg Kostin
+Version: 1.0
+Author URI: http://pmr.io
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+if ( ! function_exists ( 'wp_rest_is_front_init' ) ) :
+
+    /**
+     * Init JSON REST API isFront routes.
+     *
+     * @since 1.0.0
+     */
+    function wp_rest_is_front_init() {
+        register_rest_field(
+            'page',
+            'is_front',
+            array(
+                'get_callback' => 'wp_rest_is_front_return',
+            )
+        );
+    }
+
+    // Return settings
+    function wp_rest_is_front_return( $object, $field_name, $request ) {
+        return get_option( 'page_on_front' ) === $object['ID'];
+    }
+
+    add_action( 'init', 'wp_rest_is_front_init' );
+
+endif;
