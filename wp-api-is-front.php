79d4c5186602599c2e7a7f5427953e23480db4de
Oleg Kostin <Oleg2tor@gmail.com>
2016-03-25 01:17:25 +0300

    add: Check for WP REST API Plugin is activated

--- wp-api-is-front.php
+++ wp-api-is-front.php
@@ -9,38 +9,46 @@ Author URI: http://pmr.io
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-if ( ! function_exists ( 'wp_rest_is_front_init' ) ) :
+/**
+ * Check if WP REST API is active
+ **/
+if ( in_array( 'rest-api/plugin.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) :
 
-    /**
-     * Init JSON REST API is_front routes.
-     *
-     * @since 1.0.0
-     */
-    function wp_rest_is_front_init() {
-        register_rest_field(
-            'page',
-            'is_front',
-            array(
-                'get_callback' => 'wp_rest_is_front_return',
-            )
-        );
-    }
+    if ( ! function_exists ( 'wp_rest_is_front_init' ) ) :
 
-    /**
-     * Handler for updating page data with is_front.
-     *
-     * @since 1.0.0
-     *
-     * @param array $object The object from the response
-     * @param string $field_name Name of field
-     * @param WP_REST_Request $request Current request
-     *
-     * @return bool
-     */
-    function wp_rest_is_front_return( $object, $field_name, $request ) {
-        return (int)get_option( 'page_on_front' ) === $object['id'];
-    }
+        /**
+         * Init JSON REST API is_front routes.
+         *
+         * @since 1.0.0
+         */
+        function wp_rest_is_front_init() {
+            register_rest_field(
+                'page',
+                'is_front',
+                array(
+                    'get_callback' => 'wp_rest_is_front_return',
+                )
+            );
+        }
 
-    add_action( 'init', 'wp_rest_is_front_init' );
+        /**
+         * Handler for updating page data with is_front.
+         *
+         * @since 1.0.0
+         *
+         * @param array $object The object from the response
+         * @param string $field_name Name of field
+         * @param WP_REST_Request $request Current request
+         *
+         * @return bool
+         */
+        function wp_rest_is_front_return( $object, $field_name, $request ) {
+            return (int)get_option( 'page_on_front' ) === $object['id'];
+        }
+
+        add_action( 'init', 'wp_rest_is_front_init' );
+
+    endif;
+
 
 endif;
